956b814eb0c0babfe7e50537e7b04f8d6d0bedf0
TMD <kk412027247@gmail.com>
2019-06-08 16:04:15 +0800

    generate

---
+++ generator.py
@@ -0,0 +1,59 @@
+L = [x * x for x in range(10)]
+print(L)
+g = (x * x for x in range(10))
+
+print(g)
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+# print(next(g))
+#
+# print(next(g))
+# print(next(g))
+# print(next(g))
+
+for n in g:
+    print(n)
+
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield (b)
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+
+f = fib(6)
+print(f)
+
+
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield (3)
+    print('step 3')
+    yield (5)
+
+
+o = odd()
+next(o)
+next(o)
+next(o)
+
+for n in f:
+    print(n)
+
+g = fib(6)
+while True:
+    try:
+        x = next(g)
+        print('g: ', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
