f884ad89dce6238d9e3115137e2355e7383dd7cc
TMD <kk412027247@gmail.com>
2019-10-01 23:47:50 +0800

    @property

---
+++ oop_advance/@property.py
@@ -0,0 +1,76 @@
+class Student(object):
+    pass
+
+
+s = Student()
+s.score = 9999
+
+
+class Student(object):
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+
+    @property
+    def birth(self):
+        return self._birth
+
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2015 - self._birth
+
+
+s = Student()
+# s.set_score(60)
+
+# print(s.get_score())
+
+# s.set_score(9999)
+
+s.score = 60
+print(s.score)
+
+
+class Screen(object):
+    @property
+    def resolution(self):
+        return self._width * self._height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        self._height = height
+
+
+s = Screen()
+s.width = 1024
+s.height = 768
+print('resolution =', s.resolution)
+if s.resolution == 786432:
+    print('测试通过!')
+else:
+    print('测试失败!')
