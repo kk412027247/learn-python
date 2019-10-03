31b86dfa3e463124ae448a9c8f801eef48de0284
TMD <kk412027247@gmail.com>
2019-10-03 09:49:42 +0800

    handle error

---
+++ error/handle_error.py
@@ -0,0 +1,36 @@
+from functools import reduce
+
+try:
+    print('try...')
+    r = 10 / int('2')
+    print('result:', r)
+except ValueError as e:
+    print('ValueError:', e)
+except ZeroDivisionError as e:
+    print('ZeroDivisionError:', e)
+finally:
+    print('finally...')
+print('END')
+
+
+def str2num(s):
+    try:
+        return int(s)
+    except:
+        return float(s)
+
+
+def calc(exp):
+    ss = exp.split('+')
+    ns = map(str2num, ss)
+    return reduce(lambda acc, x: acc + x, ns)
+
+
+def main():
+    r = calc('100 + 200 + 345')
+    print('100 + 200 + 345 =', r)
+    r = calc('99 + 88+7.6')
+    print('99 + 88 + 7.6 =', r)
+
+
+main()
