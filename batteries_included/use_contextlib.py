9511212aa6c87070442bce5466b9be2f0606581e
TMD <kk412027247@gmail.com>
2019-10-04 23:25:15 +0800

    use contextlib

---
+++ batteries_included/use_contextlib.py
@@ -0,0 +1,60 @@
+from contextlib import contextmanager, closing
+from urllib.request import urlopen
+
+
+class Query(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __enter__(self):
+        print('Begin')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print('Error')
+        else:
+            print('End')
+
+    def query(self):
+        print('Query info about %s...' % self.name)
+
+
+with Query('Bob') as q:
+    q.query()
+
+
+class Query2(object):
+    def __init__(self, name):
+        self.name = name
+
+    def query(self):
+        print('Query info ablut %s...' % self.name)
+
+
+@contextmanager
+def create_query(name):
+    print('Begin')
+    q = Query2(name)
+    yield q
+    print('End')
+
+
+with create_query('Bob') as q:
+    q.query()
+
+
+@contextmanager
+def tag(name):
+    print('<%s>' % name)
+    yield
+    print('<%s>' % name)
+
+
+with tag('h1'):
+    print('hello')
+    print('suck')
+
+with closing(urlopen('https://www.python.org')) as page:
+    for line in page:
+        print(line)
