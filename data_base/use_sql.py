91a3c0828115eeb41e1480c2dc98a24bee0521e7
TMD <kk412027247@gmail.com>
2019-10-05 19:12:09 +0800

    SQLAlchemy

--- data_base/use_sql.py
+++ data_base/use_sql.py
@@ -4,7 +4,7 @@ import mysql.connector
 # 注意把password设为你的root口令:
 conn = mysql.connector.connect(user='root', password='geqhxds123', database='test')
 cursor = conn.cursor()
-# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
 cursor.execute('insert into user (id, name) values (%s, %s)', ['4', 'Michael'])
 print(cursor.rowcount)
 
