43e86b5e89531819aba3566b208540bb3fd7e1f3
TMD <kk412027247@gmail.com>
2019-10-05 20:24:10 +0800

    use wsgi

---
+++ web/wsgi.py
@@ -0,0 +1,13 @@
+from wsgiref.simple_server import make_server
+
+
+def application(environ, start_response):
+    start_response('200 ok', [('Content-Type', 'text/html')])
+    body = '<h1>Hello,%s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [b'<h1>Hello,web!</h1>']
+
+
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+
+httpd.serve_forever()
