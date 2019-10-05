556ccb99b3f0138f8995a6b914e0215777b1ca04
TMD <kk412027247@gmail.com>
2019-10-05 22:12:47 +0800

    use flask

--- web/wsgi.py
+++ web/wsgi.py
@@ -4,7 +4,7 @@ from wsgiref.simple_server import make_server
 def application(environ, start_response):
     start_response('200 ok', [('Content-Type', 'text/html')])
     body = '<h1>Hello,%s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
-    return [b'<h1>Hello,web!</h1>']
+    return [body.encode('utf-8')]
 
 
 httpd = make_server('', 8000, application)
