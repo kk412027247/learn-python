ab3be799a4abefa0e62d93582f3764b80f4be673
TMD <kk412027247@gmail.com>
2019-10-05 18:32:44 +0800

    use sql

---
+++ data_base/use_sql.py
@@ -0,0 +1,21 @@
+# 导入MySQL驱动:
+import mysql.connector
+
+# 注意把password设为你的root口令:
+conn = mysql.connector.connect(user='root', password='geqhxds123', database='test')
+cursor = conn.cursor()
+# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+cursor.execute('insert into user (id, name) values (%s, %s)', ['4', 'Michael'])
+print(cursor.rowcount)
+
+conn.commit()
+cursor.close()
+
+cursor = conn.cursor()
+cursor.execute('select * from user where id = %s', ('4',))
+values = cursor.fetchall()
+
+print(values)
+
+cursor.close()
+conn.close()
